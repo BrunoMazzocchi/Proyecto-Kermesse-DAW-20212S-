878015350fa37340974310f1c0c430573d0bc5ae
Asbell06 <73190038+Asbell06@users.noreply.github.com>
2021-11-23 12:36:37 -0600

    Última modificación

--- entidades/vw_ingresocomunidad_kermesse_comunidad_producto.php
+++ entidades/vw_ingresocomunidad_kermesse_comunidad_producto.php
@@ -5,6 +5,9 @@ class Vw_IngresoComunidad_Kermesse_Comunidad_Producto {
     private $id_kermesse;
     private $id_comunidad;
     private $id_producto;
+    private $nombreKermesse;
+    private $nombreComunidad;
+    private $nombreproducto;
     private $cant_producto;
     private $total_bonos; 
     private $usuario_creacion; 
