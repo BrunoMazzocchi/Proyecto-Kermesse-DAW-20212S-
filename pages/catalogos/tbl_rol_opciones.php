cd8bf5e905b6b6cd9603a6538dedb2819391976d
Gary Ebanks <gary.ebanks11951@est.uca.edu.ni>
2021-11-30 14:32:10 -0600

    Ajustes en sidebar

--- pages/catalogos/tbl_rol_opciones.php
+++ pages/catalogos/tbl_rol_opciones.php
@@ -108,7 +108,7 @@ if (!$acceso) {
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="../../sistema-kermesse.php" class="nav-link">Home</a>
+        <a href="../../index.html" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -153,7 +153,7 @@ if (!$acceso) {
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index.html" class="brand-link">
+    <a href="../../sistema-kermesse.php" class="brand-link">
       <img src="../../dist/img/Kermesse_Logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Kermesse</span>
     </a>
