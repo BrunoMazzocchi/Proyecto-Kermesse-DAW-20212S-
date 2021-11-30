2d5c50b9471734d4fca58ae93124686c2c52e8b1
Bruno Mazzocchi <bemazzocchi@gmail.com>
2021-11-30 14:34:55 -0600

    minor fixes

--- pages/catalogos/frm_listaprecio_det.php
+++ pages/catalogos/frm_listaprecio_det.php
@@ -216,7 +216,7 @@ $dtu = new Dt_Usuario();
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="../../index.html" class="brand-link">
+      <a href="../../sistema-kermesse.php" class="brand-link">
         <img src="../../dist/img/Kermesse_Logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Kermesse</span>
       </a>
