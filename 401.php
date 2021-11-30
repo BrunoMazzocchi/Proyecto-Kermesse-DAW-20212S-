83c22270a5a5d44166244b305eddebaef0a9e68a
Bruno Mazzocchi <bemazzocchi@gmail.com>
2021-11-29 23:35:27 -0600

    login

--- 401.php
+++ 401.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | 401 catalogo</title>
+  <title>Error | 401 catalogo</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -839,27 +839,16 @@
     <!-- Main content -->
     <section class="content">
       <div class="error-page">
-        <h2 class="headline text-warning"> 404</h2>
+        <h2 class="headline text-warning"> 401</h2>
 
         <div class="error-content">
-          <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
+          <h3><i class="fas fa-exclamation-triangle text-warning"></i> Usted no tiene acceso a esta pagina.</h3>
 
           <p>
-            We could not find the page you were looking for.
-            Meanwhile, you may <a href="../../index.html">return to dashboard</a> or try using the search form.
+           Consulte con el administrador del sistema para que le proporcione los accesos solicitados
+            Por ahora debe retornar al <a href="./sistema-kermesse.php">Inicio</a> 
           </p>
 
-          <form class="search-form">
-            <div class="input-group">
-              <input type="text" name="search" class="form-control" placeholder="Search">
-
-              <div class="input-group-append">
-                <button type="submit" name="submit" class="btn btn-warning"><i class="fas fa-search"></i>
-                </button>
-              </div>
-            </div>
-            <!-- /.input-group -->
-          </form>
         </div>
         <!-- /.error-content -->
       </div>
